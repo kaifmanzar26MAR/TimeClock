562c758c38c5b90cf321d9ebc70f42da10b57dda
Ayesha-Khatun-123 <ayeshakhatun1047@gmail.com>
2024-08-07 15:33:26 +0530

    shift added

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ const changeTime=()=>{
     // console.log(newData)
     const dateString= new Date(newData)
     // console.log(dateString)
-    const hour= dateString.getHours();
+      var hour= dateString.getHours();
     console.log(hour);
     const min= dateString.getMinutes();
     console.log(min);
     const sec= dateString.getSeconds();
     console.log(sec);
+
+    var shift=""
+
+    if(hour> 12 ){
+        shift="PM"
+        hour=hour-12;
+        console.log(hour)
+    }
+    else if(hour==12){
+        shift="PM"
+    }
+    else{
+        shift="AM"
+    }
 
     const hourTag=document.getElementById('hour');
     const minTag=document.getElementById('min');
@@ -20,6 +34,12 @@ const changeTime=()=>{
     hourTag.innerText=hour;
     minTag.innerText=min;
     secTag.innerText=sec;
+    
+
+   const shiftTime=document.getElementById('shift');
+   shiftTime.innerText=shift;
+
+
 }
 
 setInterval(changeTime, 1000);
