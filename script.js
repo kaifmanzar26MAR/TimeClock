6d739028a05e585f4fe0475fd2a09fe7c6bfabd6
MD KAIF MANZAR <kaifmanzar321@gmail.com>
2024-08-07 15:40:00 +0530

    added comments

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ const changeTime=()=>{
     // console.log(newData)
     const dateString= new Date(newData)
     // console.log(dateString)
-      var hour= dateString.getHours();
+    var hour= dateString.getHours();
     console.log(hour);
     const min= dateString.getMinutes();
     console.log(min);
     const sec= dateString.getSeconds();
     console.log(sec);
-
     var shift=""
 
     if(hour> 12 ){
@@ -20,24 +19,26 @@ const changeTime=()=>{
         hour=hour-12;
         console.log(hour)
     }
-    else if(hour==12){
+    else if(hour===12){
         shift="PM"
     }
     else{
         shift="AM"
     }
 
+    // getting tags refrence buy using the id of the tag
     const hourTag=document.getElementById('hour');
     const minTag=document.getElementById('min');
     const secTag=document.getElementById('sec');
+    const shiftTime=document.getElementById('shift');
 
+    //assigning the value of the time in the respective tags
     hourTag.innerText=hour;
     minTag.innerText=min;
     secTag.innerText=sec;
+    shiftTime.innerText=shift;
     
 
-   const shiftTime=document.getElementById('shift');
-   shiftTime.innerText=shift;
 
 
 }
